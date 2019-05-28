3249e3df4193171813091e0c44859e55f8eed2a3
Chloe Xue <chloexue18@gmail.com>
2019-05-27 21:34:03 -0500

    update problem summary

--- Hackkerrank/List Comprehension-Cuboid Coordinates.py
+++ Hackkerrank/List Comprehension-Cuboid Coordinates.py
@@ -1,5 +1,7 @@
-# -*- coding: utf-8 -*-
-
+#x,y and z representing the dimensions of a cuboid along with an integer n.
+#print a list of all possible coordinates given by (i,j,k) on a 3D grid
+#where the sum of i+j+k is not equal to n.
+#print the list in lexicographic increasing order.
 x,y,z,n = (int(input()) for _ in range(4))
 coordinates = [[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
 print(coordinates)
